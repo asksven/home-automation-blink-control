dd484df83c23243ae7637055efaed18f982fc39d
Sven Knispel <sven.knispel@gmail.com>
2018-05-21 10:56:11 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,33 @@
+# A utility to turn your blink camera on/off depending on time and on presence
+
+This utility manages your Blink camera:
+- turn on during night
+- turn on of no-one is present
+
+## Config
+
+`setenv.sh` holds the config info:
+
+```
+#!/bin/bash
+
+export BLINK_PWD=<your-blink-password>
+export BLINK_EMAIL=<your-blink-email>
+export CHECKIN_ENDPOINT=<the-endpoint-of-the-check-in-service>
+export LOCATION=<the-location-you-monitor>
+export NOTIFY=<0|1>
+export BOT_NAME=<some-name>
+export BOT_CHANNEL=<some-channel>
+export BOT_TOKEN=<slack-token>
+```
+
+To schedule the utility to run regularly (in this example every 2 minutes) edit your crontab (`crontab -e`) and add:
+```
+*/2   *    *    *    *     cd <path-to-utility> && ./arm-when-noone-present.sh
+```
+
+## Dependencies
+
+This utility depends on:
+- the check-in service: https://github.com/asksven/home-automation-checkin-service
+- `slack.phar` (https://github.com/cleentfaar/slack-cli/blob/master/README.md) if you want notifications to be sent to slack
---
+++ arm-when-noone-present.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+source setenv.sh
+
+echo running at `date` >> log
+
+# first we need to check who is at location
+curl -k $CHECKIN_ENDPOINT/checkins/$LOCATION > temp2.json
+
+RET=$?
+if [ "$RET" -ne 0 ]; then
+  echo error: curl returned $RET >> log
+  exit 1
+fi
+
+PEOPLE_PRESENT=($(jq --raw-output '. | length' temp2.json))
+
+echo people at $LOCATION $PEOPLE_PRESENT >> log
+
+CURRENT_STATUS=`cat status`
+OPERATION=""
+MESSAGE=""
+
+curl -H "Host: prod.immedia-semi.com" -H "Content-Type: application/json" --data-binary '{ "password" : "'$BLINK_PWD'", "client_specifier" : "iPhone 9.2 | 2.2 | 222", "email" : "'$BLINK_EMAIL'" }' --compressed https://rest.prod.immedia-semi.com/login > temp.json
+
+TOKEN=($(jq --raw-output .authtoken.authtoken temp.json))
+NETWORK=($(jq --raw-output '.networks | to_entries[0].key' temp.json))
+
+# part of the REST endpoint for future calls is hidden in region
+REGION=($(jq --raw-output '.region | to_entries[0].key' temp.json))
+
+echo $TOKEN
+echo $NETWORK
+echo $REGION
+
+HOUR=`date +%k%M`
+
+echo $HOUR
+
+if [ "$HOUR" -gt "0100" ]; then
+  if [ "$HOUR" -lt "0800" ]; then
+    # we are at night, arm
+    echo we are at night, arm >> log
+    OPERATION="arm"
+    MESSAGE="Night-mode. Blink armed"
+#    curl -H "Host: prod.immedia-semi.com" -H "TOKEN_AUTH: $TOKEN" --data-binary --compressed https://rest.$REGION.immedia-semi.com/network/$NETWORK/arm
+  else
+    # we are at day
+    if [ "$PEOPLE_PRESENT" = "0" ]; then
+       echo no one is present, arm >> log
+       OPERATION="arm"
+       MESSAGE="No-one present. Blink armed"
+#        curl -H "Host: prod.immedia-semi.com" -H "TOKEN_AUTH: $TOKEN" --data-binary --compressed https://rest.$REGION.immedia-semi.com/network/$NETWORK/arm
+    else
+        echo someone is present, disarm >> log
+        OPERATION="disarm"
+        MESSAGE="Day-mode and someone is present. Blink disarmed"
+#        curl -H "Host: prod.immedia-semi.com" -H "TOKEN_AUTH: $TOKEN" --data-binary --compressed https://rest.$REGION.immedia-semi.com/network/$NETWORK/disarm
+    fi
+
+  fi
+fi
+
+if [ "$OPERATION" = "$CURRENT_STATUS" ]; then
+  echo current state is $CURRENT_STATS and target is $OPERATION. NOP >>log
+else
+  echo current state is $CURRENT_STATS an target is $OPERATION. Applying $OPERATION >>log
+  curl -H "Host: prod.immedia-semi.com" -H "TOKEN_AUTH: $TOKEN" --data-binary --compressed https://rest.$REGION.immedia-semi.com/network/$NETWORK/$OPERATION
+  echo $OPERATION > status
+  if [ "$NOTIFY" -eq 1 ]; then
+  	slack.phar chat:post-message $BOT_CHANNEL "$MESSAGE" --username=$BOT_NAME --token=$BOT_TOKEN
+  fi
+fi  
+    
+
+
+
